a4ed146d4faf191d9c72a9081e4997831735afd3
Stephen Ibe <saintkalson@gmail.com>
2023-07-07 19:07:25 +0100

    integrate react-hook-form, zod for validation and create store UI

--- components/modals/store.modal.tsx
+++ components/modals/store.modal.tsx
@@ -1,10 +1,30 @@
 "use client";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
 import { Modal } from "../ui/modal";
+
+const FormSchema = z.object({
+  name: z.string().min(2, { message: "Store name is required." }),
+});
 
 const StoreModal = () => {
   const { isOpen, onClose } = useStoreModal();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
+  };
 
   return (
     <Modal
@@ -13,7 +33,26 @@ const StoreModal = () => {
       isOpen={isOpen}
       onClose={onClose}
     >
-      New Store Form
+      <>
+        <div className="space-y-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Store Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Just Buy" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+      </>
     </Modal>
   );
 };
