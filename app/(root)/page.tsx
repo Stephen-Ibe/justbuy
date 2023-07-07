59d0bb034cfeac856115b142fa73347b99a8d846
Stephen Ibe <saintkalson@gmail.com>
2023-07-07 17:09:33 +0100

    integrated Clerk Auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="p-4">
-      <Button variant="link">Click Me</Button>
+      <Button>Click Me</Button>
     </div>
   );
 }
