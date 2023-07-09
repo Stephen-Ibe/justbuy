956d90026613bb62af8f819924b1259ac07a10b2
Stephen Ibe <saintkalson@gmail.com>
2023-07-09 23:31:43 +0100

    get store name in dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,17 @@
-const Dashboard = () => <div>This is a Dashboard</div>;
+import prismadb from "@/lib/prisma.db";
+
+type TDashboardProps = {
+  params: { storeId: string };
+};
+
+const Dashboard = async ({ params }: TDashboardProps) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return <div>Active Store: {store?.name}</div>;
+};
 
 export default Dashboard;
