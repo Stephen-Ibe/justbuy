498340a4eb3bb2d7cc0f927523472c1f12a03723
Stephen Ibe <saintkalson@gmail.com>
2023-07-09 22:55:39 +0100

    dashboard layout, isAuthenticated and create store updated

--- components/modals/store.modal.tsx
+++ components/modals/store.modal.tsx
@@ -39,8 +39,7 @@ const StoreModal = () => {
       setLoading(true);
 
       const res = await axios.post("/api/stores", values);
-
-      toast.success("Store created");
+      window.location.assign(`/${res.data.id}`);
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
